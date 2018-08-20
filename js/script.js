540716deefa0824dec56bee5dc8d7a95dc673fbe
Senior Consultant <adefolarin.adeniji@andela.com>
2018-08-20 14:12:52 +0100

    Renamed date_released to year_released and changed content

--- js/script.js
+++ js/script.js
@@ -6,61 +6,61 @@ const quotes = [
     'message': 'Families are the compass that guides us. They are the inspiration to reach great heights, and our comfort when we occasionally falter.',
     'author': 'Brad Henry',
     'tag': 'Family',
-    'date_released': '22-June-2017'
+    'year_released': '2017'
   },
   {
     'message': 'Life is trying things to see if they work.',
     'author': 'Ray Bradbury',
     'tag': 'Success',
-    'date_released': '03-June-2016'
+    'year_released': '2016'
   },
   {
     'message': 'Love takes off masks that we fear we cannot live without and know we cannot live within.',
     'author': 'James Baldwin',
     'tag': 'Love',
-    'date_released': '21-08-2018'
+    'year_released': '2018'
   },
   {
     'message': 'Family is not an important thing. It’s everything.',
     'author': 'Michael J. Fox',
     'tag': 'Family',
-    'date_released': '22-June-2017'
+    'year_released': '2017'
   },
   {
     'message': 'Success is about creating value.',
     'author': 'Candice Carpenter',
     'tag': 'Success',
-    'date_released': '03-June-2016'
+    'year_released': '2016'
   },
   {
     'message': 'Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.',
     'author': 'Lucille Ball',
     'tag': 'Love',
-    'date_released': '21-08-2018'
+    'year_released': '2018'
   },
   {
     'message': 'The man of knowledge must be able not only to love his enemies but also to hate his friends.',
     'author': 'Friedrich Nietzsche',
     'tag': 'Friend',
-    'date_released': '03-June-2016'
+    'year_released': '2016'
   },
   {
     'message': 'We are shaped and fashioned by what we love.',
     'author': 'Johann Wolfgang von Goethe',
     'tag': 'Life',
-    'date_released': '03-June-2016'
+    'year_released': '2016'
   },
   {
     'message': 'A friend is someone who knows all about you and still loves you.',
     'author': 'Elbert Hubbar',
     'tag': 'Friend',
-    'date_released': '03-June-2016'
+    'year_released': '2016'
   },
   {
     'message': 'Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.',
     'author': 'Rumi',
     'tag': 'Love',
-    'date_released': '21-08-2018'
+    'year_released': '2018'
   }
 ];
 
@@ -79,7 +79,7 @@ const printQuote = () => {
   document.getElementsByClassName('quote')[0].innerHTML = quote.message;
   document.getElementsByClassName('source')[0].innerHTML = quote.author + '<span class="citation"></span><span class="year"></span>';
   document.getElementsByClassName('citation')[0].innerHTML = quote.tag;
-  document.getElementsByClassName('year')[0].innerHTML = quote.date_released;
+  document.getElementsByClassName('year')[0].innerHTML = quote.year_released;
 }
 
 
