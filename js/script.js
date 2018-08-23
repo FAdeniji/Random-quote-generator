c395f4039f5c81fe901db2635fa6108c984af93d
Senior Consultant <adefolarin.adeniji@andela.com>
2018-08-23 11:16:53 +0100

    Removing quotes around keys in an object

--- js/script.js
+++ js/script.js
@@ -3,64 +3,64 @@
 // Create the array of quote objects and name it quotes
 const quotes = [
   {
-    'message': 'Families are the compass that guides us. They are the inspiration to reach great heights, and our comfort when we occasionally falter.',
-    'author': 'Brad Henry',
-    'tag': 'Family',
-    'year_released': '2017'
+    message: 'Families are the compass that guides us. They are the inspiration to reach great heights, and our comfort when we occasionally falter.',
+    author: 'Brad Henry',
+    tag: 'Family',
+    year_released: '2017'
   },
   {
-    'message': 'Life is trying things to see if they work.',
-    'author': 'Ray Bradbury',
-    'tag': 'Success',
-    'year_released': '2016'
+    message: 'Life is trying things to see if they work.',
+    author: 'Ray Bradbury',
+    tag: 'Success',
+    year_released: '2016'
   },
   {
-    'message': 'Love takes off masks that we fear we cannot live without and know we cannot live within.',
-    'author': 'James Baldwin',
-    'tag': 'Love',
-    'year_released': '2018'
+    message: 'Love takes off masks that we fear we cannot live without and know we cannot live within.',
+    author: 'James Baldwin',
+    tag: 'Love',
+    year_released: '2018'
   },
   {
-    'message': 'Family is not an important thing. It’s everything.',
-    'author': 'Michael J. Fox',
-    'tag': 'Family',
-    'year_released': '2017'
+    message: 'Family is not an important thing. It’s everything.',
+    author: 'Michael J. Fox',
+    tag: 'Family',
+    year_released: '2017'
   },
   {
-    'message': 'Success is about creating value.',
-    'author': 'Candice Carpenter',
-    'tag': 'Success',
-    'year_released': '2016'
+    message: 'Success is about creating value.',
+    author: 'Candice Carpenter',
+    tag: 'Success',
+    year_released: '2016'
   },
   {
-    'message': 'Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.',
-    'author': 'Lucille Ball',
-    'tag': 'Love',
-    'year_released': '2018'
+    message: 'Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.',
+    author: 'Lucille Ball',
+    tag: 'Love',
+    year_released: '2018'
   },
   {
-    'message': 'The man of knowledge must be able not only to love his enemies but also to hate his friends.',
-    'author': 'Friedrich Nietzsche',
-    'tag': 'Friend',
-    'year_released': '2016'
+    message: 'The man of knowledge must be able not only to love his enemies but also to hate his friends.',
+    author: 'Friedrich Nietzsche',
+    tag: 'Friend',
+    year_released: '2016'
   },
   {
-    'message': 'We are shaped and fashioned by what we love.',
-    'author': 'Johann Wolfgang von Goethe',
-    'tag': 'Life',
-    'year_released': '2016'
+    message: 'We are shaped and fashioned by what we love.',
+    author: 'Johann Wolfgang von Goethe',
+    tag: 'Life',
+    year_released: '2016'
   },
   {
-    'message': 'A friend is someone who knows all about you and still loves you.',
-    'author': 'Elbert Hubbar',
-    'tag': 'Friend',
-    'year_released': '2016'
+    message: 'A friend is someone who knows all about you and still loves you.',
+    author: 'Elbert Hubbar',
+    tag: 'Friend',
+    year_released: '2016'
   },
   {
-    'message': 'Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.',
-    'author': 'Rumi',
-    'tag': 'Love',
-    'year_released': '2018'
+    message: 'Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.',
+    author: 'Rumi',
+    tag: 'Love',
+    year_released: '2018'
   }
 ];
 
